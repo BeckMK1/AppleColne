9d0087e50e37d4d38f9a797d256294377a3e7b7e
Mikkel Beck Knudsen <47305516+BeckMK1@users.noreply.github.com>
2023-12-12 08:59:49 +0100

    startedTreeJs

---
+++ components/Elements/Models/LaptopModel.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>
+        <primitive :object="model" />
+	</div>
+</template>
+<script setup>
+    import{useBlend} from '@tresjs/cientos'
+
+    const {scene: model} = await useBlend(
+        "@/Assets/models/ASUSLaptop.blend"
+    )
+</script>
+<style lang="scss" scoped></style>
---
+++ components/Elements/laptopShowCase.vue
@@ -0,0 +1,31 @@
+<template>
+	<div>
+        <div class="lapTop3dContainer">
+            <TresCanvas>
+                <TresPerspectiveCamera
+                    :position="[2, 2, 9]"
+                    :look-at="[0, 2, 0]"
+                />
+                <Suspense>
+                </Suspense>
+            </TresCanvas>
+        </div>
+	</div>
+</template>
+<script setup>
+    const gl = {
+        clearColor: '#F78B3D',
+        shadows: true,
+        alpha: false,
+        shadowMapType: BasicShadowMap,
+        outputColorSpace: SRGBColorSpace,
+        toneMapping: NoToneMapping,
+        windowSize: true,
+    }
+</script>
+<style lang="scss" scoped>
+    .lapTop3dContainer{
+        width: 700px;
+        height: 500px;
+    }
+</style>
